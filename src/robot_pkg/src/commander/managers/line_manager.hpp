9ebfe1f754110c645be8f5629c04dc1c8ab69388
Nicholas Ardavin <nardavin@gmail.com>
2019-12-04 17:40:10 +0000

    shortened line following dist

--- src/robot_pkg/src/commander/managers/line_manager.hpp
+++ src/robot_pkg/src/commander/managers/line_manager.hpp
@@ -4,7 +4,7 @@
 #include "../task_manager_base.hpp"
 #include "common/common_header.hpp"
 
-const double MAX_DIST = 1.6;
+const double MAX_DIST = 1.5;
 
 class LineManager : public StateMachine {
   bool start();
